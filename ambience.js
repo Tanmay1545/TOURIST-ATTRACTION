024da7ac211fcb3895f39de0a500bf7fe4f352df
tanmaymore <tanmaymorerock@gmail.com>
2024-03-19 00:56:46 +0530

    first commit

---
+++ ambience.js
@@ -0,0 +1,9 @@
+const reviewBtn = document.querySelector('#ReviewButton')
+
+reviewBtn.addEventListener('click',(e)=>{
+    const redirectTo = `https://133rg5k8-5501.inc1.devtunnels.ms/review.html` ;
+        window.location.replace(redirectTo); 
+})
+
+
+// http://192.168.56.1:8080/Reviews/Ambience%20Reviews/review.html
